66a03102f5cbb8d2af394262459cd12e5ad67ce5
Manabu Niseki <manabu.niseki@gmail.com>
2024-08-31 18:50:36 +0900

    fix: tests for dirty

--- tests/test_version_source.py
+++ tests/test_version_source.py
@@ -47,17 +47,23 @@ def test_with_format(semver_tag: TagReference):
         assert version.startswith("v1.0.0+")
 
 
-def test_with_dirty(semver_tag: TagReference):
+def test_with_dirty(repo: Repo, semver_tag: TagReference):
     source = DynamicVersionSource(str(semver_tag.repo.working_dir), {})
 
-    with patch(
-        "uv_dynamic_versioning.version_source.DynamicVersionSource.root",
-        new_callable=PropertyMock,
-    ) as mock_root:
-        mock_root.return_value = "tests/fixtures/with-dirty/"
+    repo.git.execute(["git", "commit", "--allow-empty", "-m", "empty commit"])
+    try:
+        with patch(
+            "uv_dynamic_versioning.version_source.DynamicVersionSource.root",
+            new_callable=PropertyMock,
+        ) as mock_root:
+            mock_root.return_value = "tests/fixtures/with-dirty/"
 
-        version: str = source.get_version_data()["version"]
-        assert version == "1.0.0+dirty"
+            version: str = source.get_version_data()["version"]
+            assert version.startswith("1.0.0")
+            assert version.endswith(".dirty")
+
+    finally:
+        repo.git.execute(["git", "reset", "--soft", "HEAD~1"])
 
 
 def test_with_bump(repo: Repo, semver_tag: TagReference):
@@ -74,4 +80,4 @@ def test_with_bump(repo: Repo, semver_tag: TagReference):
             version: str = source.get_version_data()["version"]
             assert version.startswith("1.0.1.")
     finally:
-        repo.git.execute(["git", "reset", "HEAD"])
+        repo.git.execute(["git", "reset", "--soft", "HEAD~1"])
