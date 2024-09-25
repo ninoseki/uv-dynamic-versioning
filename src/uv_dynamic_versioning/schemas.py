0d48657634e695ab6f037f7560610400355910b8
Manabu Niseki <manabu.niseki@gmail.com>
2024-09-25 19:34:02 +0900

    feat: support Python 3.9

--- src/uv_dynamic_versioning/schemas.py
+++ src/uv_dynamic_versioning/schemas.py
@@ -1,27 +1,29 @@
+from typing import Optional
+
 from dunamai import Pattern, Style, Vcs
 from pydantic import BaseModel, Field
 
 
 class UvDynamicVersioning(BaseModel):
     vcs: Vcs = Vcs.Any
-    metadata: bool | None = None
+    metadata: Optional[bool] = None
     tagged_metadata: bool = Field(default=False, alias="tagged-metadata")
     dirty: bool = False
     pattern: Pattern = Pattern.Default
-    pattern_prefix: str | None = Field(default=None, alias="pattern-prefix")
-    format: str | None = None
-    style: Style | None = None
+    pattern_prefix: Optional[str] = Field(default=None, alias="pattern-prefix")
+    format: Optional[str] = None
+    style: Optional[Style] = None
     latest_tag: bool = False
     strict: bool = False
     tag_dir: str = Field(default="tags", alias="tag-dir")
-    tag_branch: str | None = Field(default=None, alias="tag-branch")
+    tag_branch: Optional[str] = Field(default=None, alias="tag-branch")
     full_commit: bool = Field(default=False, alias="full-commit")
     ignore_untracked: bool = Field(default=False, alias="ignore-untracked")
     bump: bool = False
 
 
 class Tool(BaseModel):
-    uv_dynamic_versioning: UvDynamicVersioning | None = Field(
+    uv_dynamic_versioning: Optional[UvDynamicVersioning] = Field(
         default=None, alias="uv-dynamic-versioning"
     )
 
