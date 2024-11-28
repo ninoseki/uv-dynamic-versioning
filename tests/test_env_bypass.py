819e105ee3ef278816da8a5fb4a27c4616385b61
Manabu Niseki <manabu.niseki@gmail.com>
2024-11-28 19:27:08 +0900

    fix: ensuring env deletion after test

--- tests/test_env_bypass.py
+++ tests/test_env_bypass.py
@@ -1,12 +1,27 @@
 import os
 
+import pytest
+
 from uv_dynamic_versioning import schemas
 from uv_dynamic_versioning.main import get_version
 
 
-def test_get_version():
-    version = "1.1.1"
-    os.environ["UV_DYNAMIC_VERSIONING_BYPASS"] = version
-    version = get_version(schemas.UvDynamicVersioning()).unwrap()
+@pytest.fixture
+def version():
+    return "1.1.1"
 
-    assert version == version
+
+@pytest.fixture
+def set_uv_dynamic_versioning_bypass(version: str):
+    os.environ["UV_DYNAMIC_VERSIONING_BYPASS"] = version
+
+    try:
+        yield version
+    finally:
+        del os.environ["UV_DYNAMIC_VERSIONING_BYPASS"]
+
+
+@pytest.mark.usefixtures("set_uv_dynamic_versioning_bypass")
+def test_get_version(version: str):
+    got = get_version(schemas.UvDynamicVersioning()).unwrap()
+    assert got == version
