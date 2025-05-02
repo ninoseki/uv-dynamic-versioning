148b59c13bc09f7c0e7bd93dab62d15ef0620bf0
Manabu Niseki <manabu.niseki@gmail.com>
2025-05-02 09:55:33 +0900

    refactor: remove returns

--- src/uv_dynamic_versioning/base.py
+++ src/uv_dynamic_versioning/base.py
@@ -1,6 +1,5 @@
 from functools import cached_property
-
-from returns.pipeline import flow
+from typing import cast
 
 from . import schemas
 from .main import parse, read, validate
@@ -9,8 +8,9 @@ from .main import parse, read, validate
 class BasePlugin:
     @cached_property
     def project(self) -> schemas.Project:
-        root: str = self.root  # type: ignore
-        return flow(read(root), parse, validate)
+        text = read(cast(str, self.root))  # type: ignore
+        parsed = parse(text)
+        return validate(parsed)
 
     @property
     def project_config(self) -> schemas.UvDynamicVersioning:
