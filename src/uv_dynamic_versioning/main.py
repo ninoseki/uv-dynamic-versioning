c3f82382c9996f9bba9ef9f69f04d2d4c748b947
Manabu Niseki <manabu.niseki@gmail.com>
2025-05-02 10:46:01 +0900

    refactor: check version's style

--- src/uv_dynamic_versioning/main.py
+++ src/uv_dynamic_versioning/main.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
 import os
+import re
 from pathlib import Path
 
 import tomlkit
-from dunamai import Version
+from dunamai import _VALID_PEP440, _VALID_PVP, _VALID_SEMVER, Style, Version
 
 from uv_dynamic_versioning.template import render_template
 
@@ -26,6 +27,23 @@ def validate(project: tomlkit.TOMLDocument):
 
 def _get_bypassed_version() -> str | None:
     return os.environ.get("UV_DYNAMIC_VERSIONING_BYPASS")
+
+
+def check_version_style(version: str, style: Style = Style.Pep440) -> None:
+    """Check if a version is valid for a style."""
+    name, pattern = {
+        Style.Pep440: ("PEP 440", _VALID_PEP440),
+        Style.SemVer: ("Semantic Versioning", _VALID_SEMVER),
+        Style.Pvp: ("PVP", _VALID_PVP),
+    }[style]
+    failure_message = f"Version '{version}' does not conform to the {name} style"
+    if not re.search(pattern, version):
+        raise ValueError(failure_message)
+
+    if style == Style.SemVer:
+        parts = re.split(r"[.-]", version.split("+", 1)[0])
+        if any(re.search(r"^0[0-9]+$", x) for x in parts):
+            raise ValueError(failure_message)
 
 
 def _get_version(config: schemas.UvDynamicVersioning) -> Version:
@@ -56,28 +74,26 @@ def get_version(config: schemas.UvDynamicVersioning) -> tuple[str, Version]:
     version = _get_version(config)
 
     if config.format_jinja:
-        # NOTE: don't know why but poetry-dynamic-versioning check bump_config and also distance when using Jinja2
-        #       so follow it
         updated = (
-            version.bump(smart=True, index=config.bump_config.index)
+            version.bump(index=config.bump_config.index)
             if config.bump_config.enable and version.distance > 0
             else version
         )
-        return (
-            render_template(config.format_jinja, version=updated),
-            updated,
+        serialized = render_template(config.format_jinja, version=updated)
+        if config.style:
+            check_version_style(serialized, config.style)
+    else:
+        updated = (
+            version.bump(smart=True, index=config.bump_config.index)
+            if config.bump_config.enable
+            else version
+        )
+        serialized = updated.serialize(
+            metadata=config.metadata,
+            style=config.style,
+            dirty=config.dirty,
+            tagged_metadata=config.tagged_metadata,
+            format=config.format,
         )
 
-    updated = (
-        version.bump(smart=True, index=config.bump_config.index)
-        if config.bump_config.enable
-        else version
-    )
-    serialized = updated.serialize(
-        metadata=config.metadata,
-        style=config.style,
-        dirty=config.dirty,
-        tagged_metadata=config.tagged_metadata,
-        format=config.format,
-    )
     return (serialized, updated)
